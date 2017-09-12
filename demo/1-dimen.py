dcee9e9f000e0bb6919c3eee5a1815aa8b8cd5ce
Johny.Zheng <shun.johny@gmail.com>
2017-09-12 20:26:18 +0800

    update

---
+++ demo/1-dimen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+#定义常量
+rnn_unit=10       #hidden layer units
+input_size=7
+output_size=1
+lr=0.0006         #学习率
+
+
+
---
+++ demo/apt.pkg
@@ -0,0 +1,1 @@
+apt install python-tk
